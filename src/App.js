bb9a2a1eb530fbe8ff2e60ba419cb47832e63080
J Chris Kaltwasser <chris@chrisk.com>
2023-01-18 10:45:23 -0500

    added list of items

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import './App.css';
+import "./App.css";
 
-import { 
-  FoodCreateForm 
-} from './ui-components';
+// from Amplify Studio
+import { FoodCreateForm } from "./ui-components";
+
+// from tutorial : https://www.youtube.com/watch?v=DvxhI8rehtQ
+import { useEffect, useState } from "react";
+import { DataStore } from "aws-amplify";
+import { Food } from "./models";
 
 function App() {
-  return (
-    <FoodCreateForm />
-  );
+    // from (same) tutorial : https://www.youtube.com/watch?v=DvxhI8rehtQ
+    const [food, setFood] = useState([]);
+    useEffect(() => {
+        const getData = async () => {
+            await DataStore.observeQuery(Food).subscribe(({ items }) => {
+                setFood(items)
+            });
+        }
+        getData();
+    });
+
+    return (
+        <div className='container'>
+            <FoodCreateForm />
+            <ul>
+                {food.map((item) => {
+                    return (
+                        <li key={item.id}>
+                            {item.title} - {item.person}
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
